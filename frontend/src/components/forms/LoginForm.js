b5a8835e41662badef67a7855ce93d3b1b928fc5
Okko Kuisma <okkokuisma@gmail.com>
2022-09-29 15:55:11 +0300

    refactored login form to use Formik

--- frontend/src/components/forms/LoginForm.js
+++ frontend/src/components/forms/LoginForm.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Formik, Form } from 'formik'
+import * as yup from 'yup'
+
+import FormikTextInput from '../inputs/FormikTextInput'
+import { Button } from '../../styles'
 
 const LoginForm = ({ handler }) => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    setPassword('')
-    setUsername('')
-    await handler(username, password)
-  }
-
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={ handleLogin }>
-        <input type="text" value={username} id="username" name="Username" onChange={({ target }) => setUsername(target.value)}/>
-        <input type="text" value={password} id="password" name="Password" onChange={({ target }) => setPassword(target.value)}/>
-        <button type="submit">login</button>
-      </form>
-    </div>
+    <Formik
+      initialValues={{
+        username: '',
+        password: ''
+      }}
+      validationSchema={yup.object({
+        password: yup.string()
+          .required('Required')
+          .matches(
+            '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$',
+            'Password must contain a minimum of eight characters, at least one uppercase letter, one lowercase letter, one number and one special character'
+          ),
+        username: yup.string()
+          .required('Required')
+      })}
+      onSubmit={async ({username, password}) => handler(username, password)}
+    >
+      <Form>
+        <FormikTextInput
+          name='username'
+          type='text'
+          placeholder='Username'
+          label='Username'
+        />
+        <FormikTextInput
+          name='password'
+          type='password'
+          placeholder='Password'
+          label='Password'
+        />
+        <Button type='submit'>Sign in</Button>
+      </Form>
+    </Formik>
   )
 }
 
