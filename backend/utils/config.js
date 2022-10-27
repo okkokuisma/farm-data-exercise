f4ac9522c409fe187edc14ccef917b52bcc1bb96
Okko Kuisma <okkokuisma@gmail.com>
2022-10-27 13:30:02 +0300

    added a testing pipeline

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 
-const POSTGRES_USER = process.env.POSTGRES_USER
+const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres'
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
-const POSTGRES_DB = process.env.POSTGRES_DB
-const DB_HOST = process.env.DB_HOST
+const POSTGRES_DB = process.env.POSTGRES_DB || 'postgres'
+const DB_HOST = process.env.DB_HOST || 'localhost'
 const SECRET = process.env.SECRET
-const SERVER_PORT = process.env.SERVER_PORT
+const SERVER_PORT = process.env.SERVER_PORT || 3003
 
 const DB_PORT = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_DB_PORT
+  ? process.env.TEST_DB_PORT || 5432
   : process.env.DB_PORT
 
 module.exports = {
